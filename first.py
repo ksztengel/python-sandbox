a560ae831551d6d1097d1a79ac9ff32698b547da
Kristin Sztengel <kcsztengel@gmail.com>
2017-04-18 11:02:03 -0600

    basic pythong data structures and functions

---
+++ first.py
@@ -0,0 +1,17 @@
+print("Hello")
+
+greeting2 = "Hello"
+print(greeting2)
+
+# greeting3 = input("Enter a greeting:  ")
+# print(greeting3)
+
+
+a=2
+b=3
+print(a+b)
+
+
+age = input("Enter your age:  ")
+nwe_age = int(age) + 50
+print(new_age)
