ff7ef7fd5eaa108ba3a911a7d8f50b485e63f1bc
Kristin Sztengel <kcsztengel@gmail.com>
2017-04-21 08:39:30 -0600

    first script

--- exercises.py
+++ exercises.py
@@ -1,13 +1,16 @@
-exercise 1
+# exercise 1
+temperatures = [10,-20,-289,100]
 def celsius_to_farhernheit(celsius):
-    fahrenheit = (celsius*9)/5 + 32
-    return fahrenheit
+    if celsius < -273.15:
+        return "That temp doesn't make sense!"
+    else:
+        fahrenheit = (celsius*9)/5 + 32
+        return fahrenheit
+for temp in temperatures:
+    print(celsius_to_farhernheit(temp))
 
-print(celsius_to_farhernheit(20))
-
-
-exercise 2:
-print 10
+# exercise 2
+# print 10
 
 money={"saving_account":200, "checking_account":100, "under_bed":[500,10,100]}
 
---
+++ script.py
@@ -0,0 +1,11 @@
+temperatures=[10,-20,-289,100]
+
+def written(temperatures):
+    with open('temp.txt', 'w') as file:
+        for c in temperatures:
+            if c>-273.15:
+                f=c*9/5+32
+                file.write(str(f)+'\n')
+
+
+written(temperatures)
