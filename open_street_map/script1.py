e6b1683090081bb99fe180d06397b69d99af326b
Kristin Sztengel <kcsztengel@gmail.com>
2017-05-11 11:12:07 -0600

    added multiple markers using csv file

--- open_street_map/script1.py
+++ open_street_map/script1.py
@@ -1,5 +1,16 @@
+
+#import folium module
 import folium
-map1=folium.Map(location=[45.372, -121.697],zoom_start=12, tiles="Stamen Terrain")
-folium.Marker([45.3288,-121.6625],popup="Mt. Hood Meadows", icon = folium.Icon(color='red')).add_to(map1)
-folium.Marker([45.311,-121.7311],popup="Timberlake Lodge",icon = folium.Icon(color='green')).add_to(map1)
+import pandas
+
+df=pandas.read_csv("Volcanoes_USA.txt")
+
+#create object
+map1=folium.Map(location=[45.372, -121.697],zoom_start=4, tiles="Stamen Terrain")
+
+for lat,lon,name in zip(df['LAT'], df['LON'], df['NAME']):
+    folium.Marker([lat,lon],popup=name, icon = folium.Icon(color='red')).add_to(map1)
+
+
+#save to html file
 map1.save("map1.html")
