4912ed1763d593b71b3e4f1a3d28eadc7049dd96
Kristin Sztengel <kcsztengel@gmail.com>
2017-05-11 11:21:45 -0600

    marker color based on elevation

--- open_street_map/script1.py
+++ open_street_map/script1.py
@@ -8,8 +8,8 @@ df=pandas.read_csv("Volcanoes_USA.txt")
 #create object
 map1=folium.Map(location=[45.372, -121.697],zoom_start=4, tiles="Stamen Terrain")
 
-for lat,lon,name in zip(df['LAT'], df['LON'], df['NAME']):
-    folium.Marker([lat,lon],popup=name, icon = folium.Icon(color='red')).add_to(map1)
+for lat,lon,name,elev in zip(df['LAT'], df['LON'], df['NAME'], df['ELEV']):
+    folium.Marker([lat,lon],popup=name, icon = folium.Icon(color='green' if elev in range(0,1000) else "orange" if elev in range(1000,3000) else 'red')).add_to(map1)
 
 
 #save to html file
