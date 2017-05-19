e92b81ce557ebcff9190cb9dcd881f9bbf34eb5c
Kristin Sztengel <kcsztengel@gmail.com>
2017-05-19 09:18:30 -0600

    changed to absolute path

--- web_blocker/blocker.py
+++ web_blocker/blocker.py
@@ -8,7 +8,7 @@ website_list = ["www.facebook.com","facebook.com","www.instagram.com","instagram
 while True:
     if dt(dt.now().year,dt.now().month,dt.now().day,8)<dt.now()< dt(dt.now().year,dt.now().month,dt.now().day,16):
         print("Working hours....")
-        with open(hosts_temp,'r+') as file:
+        with open(hosts_path,'r+') as file:
             content=file.read()
             for website in website_list:
                 if website in content:
@@ -16,7 +16,7 @@ while True:
                 else:
                     file.write(redirect + " "+ website+"\n")
     else:
-        with open(hosts_temp,'r+') as file:
+        with open(hosts_path,'r+') as file:
             content=file.readlines()
             file.seek(0)
             for line in content:
